bbb0f331302c7b2d143f542f6fe7b8975bfc44e9
Андреичева Алина <alinkakisses@gmail.com>
2024-09-07 14:19:06 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Андреичева Алина Георгиевна. Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Алина Андреичева, ФБИ-23, 2 курс, 2023
+        </footer>
+        
+    </body>
+</html>
+"""
