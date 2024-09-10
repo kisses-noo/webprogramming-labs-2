9fc82282e9ad4e5c8bbf301f4a0039b054fd8c68
Андреичева Алина <alinkakisses@gmail.com>
2024-09-10 23:17:49 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
-    return """
-<!DOCTYPE html>
-<html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Андреичева Алина Георгиевна. Лабораторная 1</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, Лабораторная работа 1
-        </header>
+    return redirect("/menu", code=302)
 
-        <h1>web-сервер на flask</h1>
-
-        <footer>
-            &copy; Алина Андреичева, ФБИ-23, 3 курс, 2024 
-        </footer>
-        
-    </body>
-</html>
-"""
-
-@app.route("/lab1")
+@app.route("/menu")
 def lab1():
     return """
 <!DOCTYPE html>
