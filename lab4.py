4afd00157c29336a79a44b2e5f4d790cb433195a
Андреичева Алина <alinkakisses@gmail.com>
2024-11-01 00:47:36 +0700

    валидация

--- lab4.py
+++ lab4.py
@@ -103,17 +103,20 @@ def deg():
     return render_template('lab4/div5.html', x1=x1, x2=x2, result=result )
 
 tree_count = 0
+max_trees = 10
 @lab4.route('/lab4/tree', methods = ['GET', 'POST'])
 def tree():
     global tree_count
     if request.method == 'GET':
-        return render_template('lab4/tree.html', tree_count=tree_count)
+        return render_template('lab4/tree.html', tree_count=tree_count, max_trees=max_trees)
     operation = request.form.get('operation')
     
     if operation == 'cut':
-        tree_count -= 1
+        if tree_count > 0:
+            tree_count -= 1
     elif operation == 'plant':
-        tree_count += 1
-    return render_template('lab4/tree.html', tree_count = tree_count)
+        if tree_count < max_trees:
+            tree_count += 1
+    return redirect('/lab4/tree')
 
 
