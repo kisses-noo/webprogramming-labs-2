44b2fa52bd8a0c55e1ace2612f82a385ca929bc9
Андреичева Алина <alinkakisses@gmail.com>
2024-12-24 03:18:57 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
+from db import db
+from db.models import users, articles
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -6,3 +12,24 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8')
 def main():
     return render_template('lab8/lab8.html')
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error = 'Такой пользователь уже существует')
+    
+    if not login_form or not password_form:
+        return render_template('lab8/register.html', error='Заполните все поля')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8')
