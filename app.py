383eba0e69e583c2cd04f63da9f086d7b0d90efd
Андреичева Алина <alinkakisses@gmail.com>
2024-12-23 22:59:46 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from rgz import rgz
 import os
 
@@ -21,6 +22,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 app.register_blueprint(rgz)
 
 @app.route("/")
@@ -71,6 +73,10 @@ def menu():
                 
                 <li>
                     <a href="/lab7">Лабораторная работа 7</a>
+                </li>
+                
+                <li>
+                    <a href="/lab8">Лабораторная работа 8</a>
                 </li>
                 
                 <li>
