15a339f7cd1c355dfa2f646b2cb73c042f40d65a
Андреичева Алина <alinkakisses@gmail.com>
2024-12-24 20:01:50 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -33,3 +34,28 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8')
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    user = users.query.filter_by(login = login_form).first()
+    if not login_form or not password_form:
+        return render_template('lab8/login.html', error='Заполните все поля')
+    
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember= False)
+            return redirect('/lab8')
+
+    return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
+
